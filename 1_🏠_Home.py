9900f6ecb95a029b57c4dd2373bde69dd42a93b1
Suman-Adhikary <mynamesuman007@gmail.com>
2023-09-05 12:52:55 +0530

    Version 1

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -27,7 +27,7 @@ def add_bg_from_local(image_file):
         unsafe_allow_html=True
     )
 if __name__ == '__main__':
-    add_bg_from_local(".\\Dataset\\diabetes.csv")  
+    add_bg_from_local("https://github.com/Suman-Adhikary/DIABETES_CHECKING_SYSTEM/blob/main/Dataset/diabetes.csv")
 
 
 ############################################ Setup Page Header ###########################################################
