706cb21bc6ff65907ca3289771c71b8a26851336
Suman-Adhikary <mynamesuman007@gmail.com>
2023-09-12 21:18:41 +0530

    Version 1

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,8 +1,10 @@
 ########################################### Import Libraries ############################################################
+import pandas as pd
 import streamlit as st
 from pickle import load
 import base64, os
 import sys, path
+from catboost import CatBoostClassifier
 
 
 ########################################## Setup File Directory #########################################################
@@ -73,120 +75,71 @@ def process_input(input_value):
 # Age Column.
 with col1:
     AGE = st.number_input("Age", value=0)
-    AGE = process_input(AGE)
+AGE = process_input(AGE)
 
 # Bmi Column.
 with col1:
     BMI = st.number_input('Body Mass Index', value=0)
-    BMI = process_input(BMI)
+BMI = process_input(BMI)
 
 # HbA1c Column.
 with col1:
     HB = st.number_input('HbA1c Level', value=0)
-    HB = process_input(BMI)
+HB = process_input(BMI)
 
 # Blood Glucose Level Column.
 with col1:
     BGL = st.number_input('Blood Glucose Level', value=0)
-    BGL = process_input(BMI)
+BGL = process_input(BMI)
 
 # Gender column.
 with col1:
-    GENDER = st.selectbox('Gender', ('Male', 'Female', 'Others'))
-    GENDER = process_input(GENDER)
+    GENDER = st.selectbox('Gender', ('Male', 'Female'))
+GENDER = process_input(GENDER)
 
 # Hypertension Column.
 with col1:
     HT = st.selectbox('Hypertension', ('Yes', 'No'))
-    HT = process_input(HT)  
+HT = process_input(HT)  
 
 # Smoking History column.
 with col1:
-    SH = st.selectbox('Smoking History', ('Current', 'Non_Smoker', 'Past_Smoker'))
-    SH = process_input(SH)
+    SH = st.selectbox('Smoking History', ('current', 'non_smoker', 'past_smoker'))
+SH = process_input(SH)
 
 # Heart Disease column.
 with col1:
     HD = st.selectbox('Heart Disease', ('Yes', 'No'))
-    HD = process_input(HD)     
-
+HD = process_input(HD)   
+      
 
 ################################################# Conditional User Input #####################################################
 @st.cache_data
-def Conditional_Input(Age, Bmi, Hb, Bgl, Gender, Ht, Sh, Hd):
-    user_input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
-    user_input[0] = AGE
-    user_input[1] = BMI
-    user_input[2] = HB
-    user_input[3] = BGL
-
-    if GENDER == 'Male':
-        user_input[4] = 1
-        user_input[5] = 0
-        user_input[6] = 0
-    elif GENDER == 'Female':
-        user_input[4] = 0    
-        user_input[5] = 1    
-        user_input[6] = 0    
-    elif GENDER == 'Female':
-        user_input[4] = 0    
-        user_input[5] = 0    
-        user_input[6] = 1
-
+def user_input():
     if HT == 'Yes':
-        user_input[7] = 1
-        user_input[8] = 0
-    elif HT == 'No':
-        user_input[7] = 0
-        user_input[8] = 1        
-
-    if SH == 'Current':        
-        user_input[9] = 1
-        user_input[10] = 0
-        user_input[11] = 0
-    elif SH == 'Non_Smoker':        
-        user_input[9] = 0
-        user_input[10] = 1
-        user_input[11] = 0
-    elif SH == 'Past_Smoker':        
-        user_input[9] = 0
-        user_input[10] = 0
-        user_input[11] = 1
-
+        HT = 1
+    else: 
+        HT = 0
     if HD == 'Yes':
-        user_input[12] = 1
-        user_input[13] = 0
-    elif HD == 'No':  
-        user_input[12] = 0
-        user_input[13] = 1  
-    return user_input
-New_Data = Conditional_Input(AGE, BMI, HB, BGL, GENDER, HT, SH, HD)
-
-
-######################################################## Scaling New Data #########################################################
-@st.cache_data
-def Input_Scaling(Input_Data):
-    scaler = load(open('./Model And Scaling/Scale.pkl', 'rb'))
-    Input_Data = scaler.transform([Input_Data])
-    return Input_Data
-
-For_scale = New_Data[0:4]
-Apply_scaling = Input_Scaling(For_scale)
-New_Data[0:4] = Apply_scaling[0]
+        HD = 1
+    elif HD == 'No':
+        HD = 0            
+    new_data = pd.DataFrame({'age' : AGE, 'bmi' : BMI, 'HbA1c_level' : HB, 'blood_glucose_level' : BGL, 'hypertension' : 1, 'heart_disease' : HT, 'gender' : GENDER, 'smoking_history' : SH}, index = [0])
+    return new_data
 
 
 ######################################################## Predict New Data ##########################################################
 @st.cache_data
-def Prediction(Data):
-    model = load(open('./Model And Scaling/model1.pkl', 'rb'))
-    pred = model.predict([New_Data])
+def Prediction():
+    model = load(open('./Model And Scaling/CAT_BOOST.pkl', 'rb'))
+    pred = model.predict(user_input())
     return pred
 
 
 ####################################################### Button For Prediction #######################################################
 with col1:
     if st.button('Check'):
-        if Prediction == 1:
+        if Prediction()[0] == 1:
             st.write('Yes')
         else:
             st.write('No')    
